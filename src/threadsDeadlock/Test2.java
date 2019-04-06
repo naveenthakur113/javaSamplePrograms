31fd86a3e587fd5471ba560a94fffcdb16ca65e0
naveenthakur113 <mail2naveenthakur@gmail.com>
2019-04-06 01:02:32 -0400

    New file Test2

---
+++ src/threadsDeadlock/Test2.java
@@ -0,0 +1,15 @@
+package threadsDeadlock;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		MyThread1 thread1=new MyThread1(new Shared());
+		thread1.start();
+		MyThread2 thread2=new MyThread2(new Shared());
+		thread2.start();
+	}
+
+}
